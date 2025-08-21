09ce6555aac5163e4566957698b7c43ff45f5e73
divyeshmoraniya <hdivyesh62@gmail.com>
2025-08-21 11:21:41 +0530

    first commit

---
+++ app/frontend/electron/main.js
@@ -0,0 +1,33 @@
+import { app, BrowserWindow } from "electron"
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    //   win.loadFile(path.join(__dirname, "..", "react", "dist", "index.html")) // for production
+    win.loadURL("http://localhost:5174");
+
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
