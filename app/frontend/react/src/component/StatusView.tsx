b9a0134d3f1524901b674b925cca8db8247d8a1d
divyeshmoraniya <hdivyesh62@gmail.com>
2025-08-22 22:52:56 +0530

    change some ui of electron and add funcnality of checking status offline/online user

---
+++ app/frontend/react/src/component/StatusView.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+
+const StatusView: React.FC = () => {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    const updateStatus = () => {
+      const online = navigator.onLine;
+      setIsOnline(online);
+
+      if (window.electronAPI) {
+        window.electronAPI.sendOnlineStatus(online);
+      }
+    };
+
+    window.addEventListener("online", updateStatus);
+    window.addEventListener("offline", updateStatus);
+
+    updateStatus();
+
+    return () => {
+      window.removeEventListener("online", updateStatus);
+      window.removeEventListener("offline", updateStatus);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1 className="h-12 w-36">{isOnline ? "🟢 You are Online" : "🔴 You are Offline"}</h1>
+    </div>
+  );
+};
+
+export default StatusView;
